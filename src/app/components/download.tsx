3d50db01868cc87257d7bdee50e7ac5feca1d23b
Brian <brian205@mit.edu>
2023-08-10 17:50:31 -0400

    Simplified Download component code

--- src/app/components/download.tsx
+++ src/app/components/download.tsx
@@ -9,20 +9,9 @@ import styles from "../styles/projects.module.css";
  * @returns A download button.
  */
 export default function Download(props: { text: string }) {
-  const handleDownload = () => {
-    const pdfUrl = "/Resume.pdf";
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.download = "Brian_Zheng_Resume.pdf";
-    link.target = "_blank";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   return (
-    <div className={styles.button} onClick={handleDownload}>
+    <a className={styles.button} href="/Resume.pdf" download={true}>
       {props.text}
-    </div>
+    </a>
   );
 }
