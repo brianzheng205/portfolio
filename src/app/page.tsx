37446374bfc5fc9f9fd17a064c38c0a79c0f7542
Brian Zheng <brianzheng205@gmail.com>
2024-08-10 18:02:43 -0400

    Make Profile Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { useState, useEffect } from "react";
 
 import { categoryToSkills, skillToProjects, MIT } from "./data";
@@ -67,18 +68,50 @@ export default function AboutMe() {
 
   return (
     <div className={utilStyles.column}>
-      <h1>
-        I'm Brian Zheng. I'm graduating{" "}
-        <span
-          className={`${colorMIT ? styles.colorMIT : ""} ${
-            highlightMIT ? styles.highlightMIT : ""
-          }`}
-        >
-          MIT
-        </span>{" "}
-        in May 2025 with a Bachelor's of Science in Computer Science with a 4.8
-        Major GPA and a 4.7 Overall GPA.
-      </h1>
+      <div className={styles.profile}>
+        <Image
+          src="/profilePicture.png"
+          alt="Profile Picture"
+          width={200}
+          height={200}
+          className={styles["profile-picture"]}
+        />
+
+        <div className={styles.info}>
+          <div className={styles.infoItem}>
+            <h1>Name</h1>
+            <p>Brian Zheng</p>
+          </div>
+          <div className={styles.infoItem}>
+            <h1>Education</h1>
+            <p>
+              <span
+                className={`${colorMIT ? styles.colorMIT : ""} ${
+                  highlightMIT ? styles.highlightMIT : ""
+                }`}
+              >
+                MIT
+              </span>
+            </p>
+          </div>
+          <div className={styles.infoItem}>
+            <h1>Graduation Date</h1>
+            <p>May 2025</p>
+          </div>
+          <div className={styles.infoItem}>
+            <h1>Major</h1>
+            <p>Computer Science</p>
+          </div>
+          <div className={styles.infoItem}>
+            <h1>Overall GPA</h1>
+            <p>4.8</p>
+          </div>
+          <div className={styles.infoItem}>
+            <h1>Major GPA</h1>
+            <p>4.7</p>
+          </div>
+        </div>
+      </div>
 
       <div className={utilStyles.intro}>
         <div>I am looking for a 2025 New Grad Software Engineering role.</div>
